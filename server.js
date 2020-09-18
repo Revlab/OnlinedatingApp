b30124bc72e8ed2b3c19dd28449986c486cbdefa
Shirley Tinniswood <covegems@gmail.com>
2020-09-18 14:41:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 //environment variable for port
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 //setup view engine
 app.engine('handlebars',exphbs({defaultLayout:'main'}))
 app.set('view engine','handlebars')
